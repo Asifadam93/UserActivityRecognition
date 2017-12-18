e858de4514c09c57cd7a5ee07c370ed2efedd73e
PetitGrigri <fgriselles@hotmail.fr>
2017-12-18 01:19:26 +0100

    Ajout du bouton d'enregistrement qui lance un timer de 4s

--- Android/app/src/main/java/com/example/asif/useractivityrecognition/MainActivity.java
+++ Android/app/src/main/java/com/example/asif/useractivityrecognition/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.asif.useractivityrecognition;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.asif.useractivityrecognition.Save.SaveSampleUtils;
@@ -25,6 +28,15 @@ public class MainActivity extends AppCompatActivity implements OnRequestPermissi
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        Button validateButton =  this.findViewById(R.id.validateButtonView);
+
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RecordActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //Récupération de la récupération d'écrire sur
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
--- Android/app/src/main/java/com/example/asif/useractivityrecognition/RecordActivity.java
+++ Android/app/src/main/java/com/example/asif/useractivityrecognition/RecordActivity.java
@@ -1,13 +1,25 @@
 package com.example.asif.useractivityrecognition;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.asif.useractivityrecognition.Save.SaveSampleUtils;
 
 public class RecordActivity extends AppCompatActivity {
+
+    private static final String TAG = RecordActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +28,50 @@ public class RecordActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final ProgressBar progress  = (ProgressBar) this.findViewById(R.id.progressBar);
+        final TextView timer        = (TextView) this.findViewById(R.id.timerView);
+        final Button buttonReccord  = (Button) this.findViewById(R.id.buttonReccord);
+
+        final Handler handler = new Handler();
+        progress.setProgress(0);
+
+
+        final View.OnClickListener clickReccord= new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                buttonReccord.setClickable(false);
+                Log.i(TAG, String.valueOf("onKey"));
+
+                buttonReccord.setPressed(true);
+                new CountDownTimer(4000, 100) {
+
+                    public void onTick(long millisUntilFinished) {
+
+                        int msLeft = (((4000 - (int) millisUntilFinished)));
+                        int secondLeft = (int) (msLeft / 1000);
+
+                        int i = (msLeft * 100 / 4000);
+
+                        timer.setText(String.valueOf(secondLeft) + ":" + String.valueOf(Math.abs((secondLeft * 1000) - msLeft)));
+                        progress.setProgress(i);
+
+                        Log.i(TAG, String.valueOf(i));
+                    }
+
+                    public void onFinish() {
+                        timer.setText("0.000");
+                        progress.setProgress(0);
+                        buttonReccord.setClickable(true);
+                    }
+                }.start();
             }
-        });
+        };
+
+        buttonReccord.setOnClickListener(clickReccord);
+
+
+
+
+
     }
 }
