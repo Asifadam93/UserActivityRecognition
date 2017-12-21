e7e2decd8ce0cb44a55174b585fdeb38410d902c
Unknown <adamsha.asif@gmail.com>
2017-12-21 07:21:49 +0100

    GeoFencing activity edits

--- Android/app/src/main/java/com/example/asif/useractivityrecognition/GeoFencingActivity.java
+++ Android/app/src/main/java/com/example/asif/useractivityrecognition/GeoFencingActivity.java
@@ -1,18 +1,45 @@
 package com.example.asif.useractivityrecognition;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 public class GeoFencingActivity extends AppCompatActivity {
+
+    private static String TAG = "mGeoFencingActivity";
+    private MapFragment mapFragment;
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private static int REQ_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_fencing);
+
+        Log.i(TAG, "onCreate");
+
+        // custom inits
+        initMap();
+        initGoogleApi();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +51,88 @@ public class GeoFencingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+        mGoogleApiClient.disconnect();
+    }
+
+    private void initMap() {
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Log.i(TAG, "Map ready");
+            }
+        });
+    }
+
+    private void initGoogleApi() {
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                        @Override
+                        public void onConnected(@Nullable Bundle bundle) {
+                            Log.i(TAG, "Google api connected");
+                            getLastLocation();
+                        }
+
+                        @Override
+                        public void onConnectionSuspended(int i) {
+                            Log.i(TAG, "Google api connection suspended");
+                        }
+                    })
+                    .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                        @Override
+                        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                            Log.i(TAG, "Google api connection failed");
+                        }
+                    })
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    private void getLastLocation(){
+
+        Log.i(TAG, "getLastLocation");
+
+        if(checkLocationPermission()){
+
+        } else {
+            askPermission();
+        }
+
+    }
+
+    private boolean checkLocationPermission(){
+
+        Log.i(TAG, "checkLocationPermission");
+
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED );
+    }
+
+    private void askPermission(){
+
+        Log.i(TAG, "askPermission");
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                REQ_PERMISSION
+        );
+
     }
 
 }
